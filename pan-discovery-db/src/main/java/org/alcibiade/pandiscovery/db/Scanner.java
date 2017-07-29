5eec9f1f082b8833f61ae034e55a596509bc9dfc
Yannick Kirschhoffer <alcibiade@alcibiade.org>
2017-07-29 22:37:55 +0200

    #21: Display root cause of runtime errors in the console.

--- pan-discovery-db/src/main/java/org/alcibiade/pandiscovery/db/Scanner.java
+++ pan-discovery-db/src/main/java/org/alcibiade/pandiscovery/db/Scanner.java
@@ -1,6 +1,8 @@
 package org.alcibiade.pandiscovery.db;
 
 import org.alcibiade.pandiscovery.db.command.DiscoveryCommand;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -17,9 +19,17 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class Scanner {
 
     public static void main(String[] args) throws Exception {
+        Logger logger = LoggerFactory.getLogger(Scanner.class);
+
         try (ConfigurableApplicationContext context = SpringApplication.run(Scanner.class, args)) {
             DiscoveryCommand command = context.getBean(DiscoveryCommand.class);
             command.runScan();
+        } catch (Throwable t) {
+            while (t.getCause() != null) {
+                t = t.getCause();
+            }
+
+            logger.info(t.getLocalizedMessage());
         }
     }
 }
