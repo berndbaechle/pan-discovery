5d9b9e9aed2c528917f3244d4e95171b49d6e70d
Yannick Kirschhoffer <alcibiade@alcibiade.org>
2017-07-30 00:38:51 +0200

    Remove previous version of jar files in docker build.

--- scripts/build-docker-images.sh
+++ scripts/build-docker-images.sh
@@ -8,6 +8,7 @@ DOCKER_DIR=${BASE_DIR}/docker
 cd ${BASE_DIR}
 ./mvnw -Poracle -Pmysql -Ppostgres clean install
 
+rm -f ${DOCKER_DIR}/*/*.jar
 cp ${BASE_DIR}/pan-discovery-db/target/pan-discovery-db-*.jar ${DOCKER_DIR}/db/
 cp ${BASE_DIR}/pan-discovery-fs/target/pan-discovery-fs-*.jar ${DOCKER_DIR}/fs/
 
