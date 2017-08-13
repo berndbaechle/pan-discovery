51301d9b4433391d2a5cb4715c13e42a1bec884e
Yannick Kirschhoffer <alcibiade@alcibiade.org>
2017-08-13 11:49:09 +0200

    Update the docker build script to provide explicit tag name as parameter.

--- scripts/build-docker-images.sh
+++ scripts/build-docker-images.sh
@@ -4,6 +4,13 @@ set -e
 
 BASE_DIR=$(readlink -f $(dirname $0)/..)
 DOCKER_DIR=${BASE_DIR}/docker
+DOCKER_TAG=${1}
+
+if test "${DOCKER_TAG}" == ""
+then
+	echo "A docker tag must be provided as argument"
+	exit 1
+fi
 
 cd ${BASE_DIR}
 ./mvnw -Poracle -Pmysql -Ppostgres clean install
@@ -13,8 +20,8 @@ cp ${BASE_DIR}/pan-discovery-db/target/pan-discovery-db-*.jar ${DOCKER_DIR}/db/
 cp ${BASE_DIR}/pan-discovery-fs/target/pan-discovery-fs-*.jar ${DOCKER_DIR}/fs/
 
 cd ${DOCKER_DIR}/db/
-docker build -t alcibiade/pan-discovery-db .
+docker build -t alcibiade/pan-discovery-db:${DOCKER_TAG} .
 
 cd ${DOCKER_DIR}/fs/
-docker build -t alcibiade/pan-discovery-fs .
+docker build -t alcibiade/pan-discovery-fs:${DOCKER_TAG} .
 
